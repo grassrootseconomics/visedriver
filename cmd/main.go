a096d3f5b529a2fb2f1e6d7b3fa687ebbd8e556f
alfred-mk <alfredmwaik@gmail.com>
2024-08-15 14:58:44 +0300

    change to use registration as the entry point

--- cmd/main.go
+++ cmd/main.go
@@ -49,7 +49,7 @@ func main() {
 	var sessionId string
 	flag.StringVar(&dir, "d", ".", "resource dir to read from")
 	flag.UintVar(&size, "s", 0, "max size of output")
-	flag.StringVar(&root, "root", "root", "entry point symbol")
+	flag.StringVar(&root, "root", "registration", "entry point symbol")
 	flag.StringVar(&sessionId, "session-id", "default", "session id")
 	flag.Parse()
 	fmt.Fprintf(os.Stderr, "starting session at symbol '%s' using resource dir: %s\n", root, dir)
@@ -59,7 +59,7 @@ func main() {
 	rfs := resource.NewFsResource(scriptDir)
 	ca := cache.NewCache()
 	cfg := engine.Config{
-		Root:      "root",
+		Root:      "registration",
 		SessionId: sessionId,
 	}
 

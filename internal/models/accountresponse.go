ee649dc79e7af6a7c2d2e01ce25ac44f07ec4269
Carlosokumu <carlosokumu254@gmail.com>
2024-08-26 12:10:39 +0300

    setup models for handling api responses

---
+++ internal/models/accountresponse.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+)
+
+
+type trackStatusResponse struct {
+	Ok     bool `json:"ok"`
+	Result struct {
+		Transaction struct {
+			CreatedAt     time.Time   `json:"createdAt"`
+			Status        string      `json:"status"`
+			TransferValue json.Number `json:"transferValue"`
+			TxHash        string      `json:"txHash"`
+			TxType        string      `json:"txType"`
+		}
+	} `json:"result"`
+}

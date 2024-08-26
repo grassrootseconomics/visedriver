d86de39b390b4148c535467d49124de159f1f91f
Carlosokumu <carlosokumu254@gmail.com>
2024-08-26 13:38:14 +0300

    define the api endpoints

--- config/config.go
+++ config/config.go
@@ -2,5 +2,9 @@ package config
 
 
 
+const (
+    CreateAccountURL = "https://custodial.sarafu.africa/api/account/create"
+    TrackStatusURL   = "https://custodial.sarafu.africa/api/track/"
+    BalanceURL       = "https://custodial.sarafu.africa/api/account/status/"
+)
 
-//We define our endpoints here?

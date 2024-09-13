123fdec009fcf2bf87c75278cfbde716500670ff
Carlosokumu <carlosokumu254@gmail.com>
2024-09-13 18:04:03 +0300

    clean up account service

--- internal/handlers/server/accountservice.go
+++ internal/handlers/server/accountservice.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -19,8 +20,6 @@ type AccountService struct {
 	Client *http.Client
 }
 
-
-
 // CheckAccountStatus retrieves the status of an account transaction based on the provided tracking ID.
 //
 // Parameters:
@@ -28,14 +27,12 @@ type AccountService struct {
 //     CreateAccount or a similar function that returns an AccountResponse. The `trackingId` field in the
 //     AccountResponse struct can be used here to check the account status during a transaction.
 //
-//
 // Returns:
 //   - string: The status of the transaction as a string. If there is an error during the request or processing, this will be an empty string.
 //   - error: An error if any occurred during the HTTP request, reading the response, or unmarshalling the JSON data.
 //     If no error occurs, this will be nil.
-//
 func (as *AccountService) CheckAccountStatus(trackingId string) (string, error) {
-	resp,err := as.Client.Get(config.TrackStatusURL + trackingId)
+	resp, err := as.Client.Get(config.TrackStatusURL + trackingId)
 	// resp, err := http.Get(config.TrackStatusURL + trackingId)
 	if err != nil {
 		return "", err
@@ -52,20 +49,18 @@ func (as *AccountService) CheckAccountStatus(trackingId string) (string, error) 
 	if err != nil {
 		return "", err
 	}
+	fmt.Println("hash:", trackResp.Result.Transaction.TxHash)
 
 	status := trackResp.Result.Transaction.Status
 
 	return status, nil
 }
 
-
 // CheckBalance retrieves the balance for a given public key from the custodial balance API endpoint.
 // Parameters:
 //   - publicKey: The public key associated with the account whose balance needs to be checked.
 func (as *AccountService) CheckBalance(publicKey string) (string, error) {
-
-	//resp, err := http.Get(config.BalanceURL + publicKey)
-	resp, err := as.Client.Get(config.BalanceURL + publicKey)
+	resp, err := http.Get(config.BalanceURL + publicKey)
 	if err != nil {
 		return "0.0", err
 	}
@@ -86,15 +81,13 @@ func (as *AccountService) CheckBalance(publicKey string) (string, error) {
 	return balance, nil
 }
 
-
-//CreateAccount creates a new account in the custodial system.
+// CreateAccount creates a new account in the custodial system.
 // Returns:
 //   - *models.AccountResponse: A pointer to an AccountResponse struct containing the details of the created account.
 //     If there is an error during the request or processing, this will be nil.
 //   - error: An error if any occurred during the HTTP request, reading the response, or unmarshalling the JSON data.
 //     If no error occurs, this will be nil.
 func (as *AccountService) CreateAccount() (*models.AccountResponse, error) {
-	//resp, err := http.Post(config.CreateAccountURL, "application/json", nil)
 	resp, err := as.Client.Post(config.CreateAccountURL, "application/json", nil)
 	if err != nil {
 		return nil, err
@@ -111,6 +104,5 @@ func (as *AccountService) CreateAccount() (*models.AccountResponse, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	return &accountResp, nil
 }

be165e50337dc5ed52237992f8c165c54d76a11d
alfred-mk <alfredmwaik@gmail.com>
2024-09-30 16:05:14 +0300

    added tests and test cases

--- menu_traversal_test.go
+++ menu_traversal_test.go
@@ -78,3 +78,57 @@ func TestTerms(t *testing.T) {
 		}
 	}
 }
+
+func TestAccountRegistrationRejectTerms(t *testing.T) {
+	en, _ := enginetest.TestEngine("session1234112")
+	var err error
+	ctx := context.Background()
+	sessions := testData
+	for _, session := range sessions {
+		groups := driver.FilterGroupsByName(session.Groups, "account_creation_reject_terms")
+		for _, group := range groups {
+			for _, step := range group.Steps {
+				cont, _ := en.Exec(ctx, []byte(step.Input))
+				if cont {
+					w := bytes.NewBuffer(nil)
+					_, err = en.Flush(ctx, w)
+					if err != nil {
+						t.Fatal(err)
+					}
+					b := w.Bytes()
+					if !bytes.Equal(b, []byte(step.ExpectedContent)) {
+						t.Fatalf("expected:\n\t%s\ngot:\n\t%s\n", step.ExpectedContent, b)
+					}
+
+				}
+			}
+		}
+	}
+}
+
+func TestAccountRegistrationInvalidPin(t *testing.T) {
+	en, _ := enginetest.TestEngine("session1234112")
+	var err error
+	ctx := context.Background()
+	sessions := testData
+	for _, session := range sessions {
+		groups := driver.FilterGroupsByName(session.Groups, "account_creation_invalid_pin")
+		for _, group := range groups {
+			for _, step := range group.Steps {
+				cont, _ := en.Exec(ctx, []byte(step.Input))
+				if cont {
+					w := bytes.NewBuffer(nil)
+					_, err = en.Flush(ctx, w)
+					if err != nil {
+						t.Fatal(err)
+					}
+					b := w.Bytes()
+					if !bytes.Equal(b, []byte(step.ExpectedContent)) {
+						t.Fatalf("expected:\n\t%s\ngot:\n\t%s\n", step.ExpectedContent, b)
+					}
+
+				}
+			}
+		}
+	}
+}

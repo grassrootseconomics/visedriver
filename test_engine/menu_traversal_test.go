d94037d4998fb1f2a554297ec62ebafaaa86d219
alfred-mk <alfredmwaik@gmail.com>
2024-10-03 14:07:36 +0300

    combine the invalid PIN case and remove unused tests

--- test_engine/menu_traversal_test.go
+++ test_engine/menu_traversal_test.go
@@ -496,37 +496,6 @@ func TestMyAccount_Check_My_Balance(t *testing.T) {
 	}
 }
 
-func TestMyAccount_Check_My_Balance_IncorrectPin(t *testing.T) {
-	en, fn := enginetest.TestEngine("session1234112")
-	defer fn()
-	ctx := context.Background()
-	sessions := testData
-	for _, session := range sessions {
-		groups := driver.FilterGroupsByName(session.Groups, "menu_my_account_check_my_balance_incorrect_pin")
-		for _, group := range groups {
-			for index, step := range group.Steps {
-				t.Logf("step %v with input %v", index, step.Input)
-				cont, err := en.Exec(ctx, []byte(step.Input))
-				if err != nil {
-					t.Errorf("Test case '%s' failed at input '%s': %v", group.Name, step.Input, err)
-					return
-				}
-				if !cont {
-					break
-				}
-				w := bytes.NewBuffer(nil)
-				if _, err := en.Flush(ctx, w); err != nil {
-					t.Errorf("Test case '%s' failed during Flush: %v", group.Name, err)
-				}
-				b := w.Bytes()
-				if !bytes.Equal(b, []byte(step.ExpectedContent)) {
-					t.Fatalf("expected:\n\t%s\ngot:\n\t%s\n", step.ExpectedContent, b)
-				}
-			}
-		}
-	}
-}
-
 func TestMyAccount_Check_Community_Balance(t *testing.T) {
 	en, fn := enginetest.TestEngine("session1234112")
 	defer fn()
@@ -534,37 +503,6 @@ func TestMyAccount_Check_Community_Balance(t *testing.T) {
 	sessions := testData
 	for _, session := range sessions {
 		groups := driver.FilterGroupsByName(session.Groups, "menu_my_account_check_community_balance")
-		for _, group := range groups {
-			for index, step := range group.Steps {
-				t.Logf("step %v with input %v", index, step.Input)
-				cont, err := en.Exec(ctx, []byte(step.Input))
-				if err != nil {
-					t.Errorf("Test case '%s' failed at input '%s': %v", group.Name, step.Input, err)
-					return
-				}
-				if !cont {
-					break
-				}
-				w := bytes.NewBuffer(nil)
-				if _, err := en.Flush(ctx, w); err != nil {
-					t.Errorf("Test case '%s' failed during Flush: %v", group.Name, err)
-				}
-				b := w.Bytes()
-				if !bytes.Equal(b, []byte(step.ExpectedContent)) {
-					t.Fatalf("expected:\n\t%s\ngot:\n\t%s\n", step.ExpectedContent, b)
-				}
-			}
-		}
-	}
-}
-
-func TestMyAccount_Check_Community_Balance_IncorrectPin(t *testing.T) {
-	en, fn := enginetest.TestEngine("session1234112")
-	defer fn()
-	ctx := context.Background()
-	sessions := testData
-	for _, session := range sessions {
-		groups := driver.FilterGroupsByName(session.Groups, "menu_my_account_check_community_balance_incorrect_pin")
 		for _, group := range groups {
 			for index, step := range group.Steps {
 				t.Logf("step %v with input %v", index, step.Input)

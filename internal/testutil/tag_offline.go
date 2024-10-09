c6227acba169f247bfa60c9d2493ff0a7d439f08
Carlosokumu <carlosokumu254@gmail.com>
2024-10-09 16:46:22 +0300

    use build tag to define account service

---
+++ internal/testutil/tag_offline.go
@@ -0,0 +1,13 @@
+// +build !online
+
+package testutil
+
+import (
+	"git.grassecon.net/urdt/ussd/internal/handlers/server"
+)
+
+var AccountService server.AccountServiceInterface
+
+func init() {
+	AccountService = &server.MockAccountService{}
+}

b8e12e5215ec48c49dc7673a0dd9509a155df5e1
Carlosokumu <carlosokumu254@gmail.com>
2024-10-18 17:01:21 +0300

    update api endpoints

--- config/config.go
+++ config/config.go
@@ -1,10 +1,7 @@
 package config
 
-
-
 const (
-    CreateAccountURL = "https://custodial.sarafu.africa/api/account/create"
-    TrackStatusURL   = "https://custodial.sarafu.africa/api/track/"
-    BalanceURL       = "https://custodial.sarafu.africa/api/account/status/"
+	CreateAccountURL = "http://localhost:5003/api/v2/account/create"
+	BalanceURL       = "https://custodial.sarafu.africa/api/account/status/"
+	TrackURL         = "http://localhost:5003/api/v2/account/status"
 )
-

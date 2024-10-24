c796bbdcfcc535cd05fc14d552c0d02dea27a996
Carlosokumu <carlosokumu254@gmail.com>
2024-10-24 16:36:09 +0300

    correct create endpoint

--- config/config.go
+++ config/config.go
@@ -11,7 +11,7 @@ var (
 
 // LoadConfig initializes the configuration values after environment variables are loaded.
 func LoadConfig() {
-	CreateAccountURL = initializers.GetEnv("CREATE_ACCOUNT_URL", "http://localhost:5003/api/v2/account/creates")
+	CreateAccountURL = initializers.GetEnv("CREATE_ACCOUNT_URL", "http://localhost:5003/api/v2/account/create")
 	TrackStatusURL = initializers.GetEnv("TRACK_STATUS_URL", "https://custodial.sarafu.africa/api/track/")
 	BalanceURL = initializers.GetEnv("BALANCE_URL", "https://custodial.sarafu.africa/api/account/status/")
     TrackURL = initializers.GetEnv("TRACK_URL", "http://localhost:5003/api/v2/account/status")

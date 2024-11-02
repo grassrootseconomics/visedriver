dae12ac4989e40f64a48930b5348288c85af8369
lash <dev@holbrook.no>
2024-11-02 23:41:08 +0000

    Separate subprefix db export

--- common/storage.go
+++ common/storage.go
@@ -2,12 +2,14 @@ package common
 
 import (
 	"git.defalsify.org/vise.git/db"
+
+	"git.grassecon.net/urdt/ussd/internal/storage"
 )
 
-func StoreToDb(store *UserDataStore, prefix []byte) db.Db {
-	innerStore := store.Db
-	if pfx != nil {
-		innerStore = NewSubPrefixDb(innerStore, pfx)		
-	}
-	return innerStore
+func StoreToDb(store *UserDataStore) db.Db {
+	return store.Db
+}
+
+func StoreToPrefixDb(store *UserDataStore, pfx []byte) storage.PrefixDb {
+	return storage.NewSubPrefixDb(store.Db, pfx)	
 }

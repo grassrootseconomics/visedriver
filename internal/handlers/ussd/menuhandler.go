b8d938d3aa99ddecd999a2a68cf6b326b8f9b71f
Carlosokumu <carlosokumu254@gmail.com>
2024-11-21 13:04:19 +0300

    add voucher details

--- internal/handlers/ussd/menuhandler.go
+++ internal/handlers/ussd/menuhandler.go
@@ -1626,10 +1626,9 @@ func (h *Handlers) GetVoucherDetails(ctx context.Context, sym string, input []by
 		return res, nil
 	}
 
-	tokenSymbol := voucherData.TokenSymbol
-	tokenName := voucherData.TokenName
-
-	res.Content = fmt.Sprintf("%s %s", tokenSymbol, tokenName)
+	res.Content = fmt.Sprintf(
+		"name: %s\nsymbol: %s\ncommodity: %s\nlocation: %s", voucherData.TokenName, voucherData.TokenSymbol, voucherData.TokenCommodity, voucherData.TokenLocation,
+	)
 
 	return res, nil
 }

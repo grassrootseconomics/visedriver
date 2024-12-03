d40a4a171ff1bb84b7cc299e13f3b04836cdb9bd
alfred-mk <alfredmwaik@gmail.com>
2024-12-03 14:12:47 +0300

    formatted code

--- internal/utils/name.go
+++ internal/utils/name.go
@@ -1,17 +1,17 @@
 package utils
 
 func ConstructName(firstName, familyName, defaultValue string) string {
-    name := defaultValue
-    if familyName != defaultValue {
-        if firstName != defaultValue {
-            name = firstName + " " + familyName
-        } else {
-            name = familyName
-        }
-    } else {
-        if firstName != defaultValue {
-            name = firstName
-        }
-    }
-    return name
+	name := defaultValue
+	if familyName != defaultValue {
+		if firstName != defaultValue {
+			name = firstName + " " + familyName
+		} else {
+			name = familyName
+		}
+	} else {
+		if firstName != defaultValue {
+			name = firstName
+		}
+	}
+	return name
 }

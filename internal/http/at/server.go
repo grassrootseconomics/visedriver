3ed9caf16d3e903b72de8e967a34cd5013be6104
lash <dev@holbrook.no>
2025-01-04 08:02:44 +0000

    Factor out request parsers

--- internal/http/at/server.go
+++ internal/http/at/server.go
@@ -1,4 +1,4 @@
-package http
+package at
 
 import (
 	"io"
--- internal/http/server.go
+++ internal/http/server.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"io/ioutil"
 	"net/http"
 	"strconv"
 
@@ -13,34 +12,6 @@ import (
 var (
 	logg = logging.NewVanilla().WithDomain("httpserver")
 )
-
-type DefaultRequestParser struct {
-}
-
-func (rp *DefaultRequestParser) GetSessionId(rq any) (string, error) {
-	rqv, ok := rq.(*http.Request)
-	if !ok {
-		return "", handlers.ErrInvalidRequest
-	}
-	v := rqv.Header.Get("X-Vise-Session")
-	if v == "" {
-		return "", handlers.ErrSessionMissing
-	}
-	return v, nil
-}
-
-func (rp *DefaultRequestParser) GetInput(rq any) ([]byte, error) {
-	rqv, ok := rq.(*http.Request)
-	if !ok {
-		return nil, handlers.ErrInvalidRequest
-	}
-	defer rqv.Body.Close()
-	v, err := ioutil.ReadAll(rqv.Body)
-	if err != nil {
-		return nil, err
-	}
-	return v, nil
-}
 
 type SessionHandler struct {
 	handlers.RequestHandler
